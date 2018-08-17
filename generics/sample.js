7abea0949d265dd3545e327fa4d3c92b7641af51
yinm <yinm.cminor9@gmail.com>
2018-08-17 16:08:11 +0900

    Prepare

---
+++ generics/sample.js
@@ -0,0 +1,6 @@
+var getStringArray = function (value) {
+    return [value, value, value];
+};
+var getNumberArray = function (value) {
+    return [value, value, value];
+};
---
+++ generics/sample.ts
@@ -0,0 +1,7 @@
+let getStringArray = (value: string): string[] => {
+  return [value, value, value]
+}
+
+let getNumberArray = (value: number): number[] => {
+  return [value, value, value]
+}
