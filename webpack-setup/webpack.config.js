36b1c9c009d0231638d62a8b9d586ab7ebad381a
yinm <yinm.cminor9@gmail.com>
2018-08-26 15:23:25 +0900

    Add settings of webpack

---
+++ webpack-setup/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'development',
+
+  entry: './src/main.ts',
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: [
+      '.ts'
+    ]
+  }
+}
