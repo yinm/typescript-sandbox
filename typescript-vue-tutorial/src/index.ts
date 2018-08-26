1029a08ba043ba04a96d2539c89ac2c134ebdd05
yinm <yinm.cminor9@gmail.com>
2018-08-26 17:35:45 +0900

    Add a component

--- typescript-vue-tutorial/src/index.ts
+++ typescript-vue-tutorial/src/index.ts
@@ -1,13 +1,18 @@
 import Vue from 'vue'
+import HelloComponent from './components/Hello'
 
 let v = new Vue({
   el: '#app',
   template: `
-  <div>
-    <div>Hello {{name}}!</div>
-    Name: <input v-model="name" type="text">
-  </div>`,
+    <div>
+      Name: <input v-model="name" type="text">
+      <hello-component :name="name" :initialEnthusiasm="5" />
+    </div>
+  `,
   data: {
     name: 'world'
+  },
+  components: {
+    HelloComponent
   }
 })
