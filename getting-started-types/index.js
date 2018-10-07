54068efbfc07afe65a69b564b59187bb80bbf109
yinm <yinm.cminor9@gmail.com>
2018-10-07 11:58:39 +0900

    Check union type

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,15 +1,5 @@
 "use strict";
-function bind(func, value) {
-    return function () {
-        var args = [];
-        for (var _i = 0; _i < arguments.length; _i++) {
-            args[_i] = arguments[_i];
-        }
-        return func.apply(void 0, [value].concat(args));
-    };
-}
-var add = function (x, y) { return x + y; };
-var add1 = bind(add, 1);
-console.log(add1(5));
-console.log(add1(5));
-add1('foo');
+var value = 'foo';
+value = 100;
+value = 'bar';
+value = true;
