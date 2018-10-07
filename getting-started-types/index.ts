9958fb7d70d42fc3a242ad34a7ed683a1f25fcef
yinm <yinm.cminor9@gmail.com>
2018-10-07 13:51:24 +0900

    Check ? modifier

--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,27 +1,13 @@
-interface Hoge {
-  type: 'hoge'
+interface MyObj {
   foo: string
+  bar?: number
 }
 
-interface Piyo {
-  type: 'piyo'
-  bar: number
+let obj: MyObj = {
+  foo: 'string'
 }
 
-interface Fuga {
-  baz: boolean
+obj = {
+  foo: 'foo',
+  bar: 100,
 }
-
-type Obj = (Hoge | Piyo) & Fuga
-
-function func(obj: Obj) {
-  console.log(obj.baz)
-  if (obj.type === 'hoge') {
-    console.log(obj.foo)
-  } else {
-    console.log(obj.bar)
-  }
-}
-
-func({type: 'hoge', foo: 'aaa', baz: true})
-func({type: 'piyo', bar: 1, baz: true})
