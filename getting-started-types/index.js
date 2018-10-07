0e3a12e2fd097d62a647d522406c234331f69cda
yinm <yinm.cminor9@gmail.com>
2018-10-07 11:10:03 +0900

    Check void

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,11 +1,3 @@
 "use strict";
-var func = function (foo) {
-    var bar = [];
-    for (var _i = 1; _i < arguments.length; _i++) {
-        bar[_i - 1] = arguments[_i];
-    }
-    return bar;
-};
-console.log(func('foo'));
-console.log(func('bar', 1, 2, 3));
-console.log(func('baz', 'hey', 2, 3));
+var a = undefined;
+var b = a;
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,5 +1,2 @@
-const func = (foo: string, ...bar: number[]) => bar
-
-console.log(func('foo'))
-console.log(func('bar', 1, 2, 3))
-console.log(func('baz', 'hey', 2, 3))
+const a: void = undefined
+const b: undefined = a
