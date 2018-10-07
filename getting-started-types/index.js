621e0a4683bf70d7f83486523c2d17953d11a7f6
yinm <yinm.cminor9@gmail.com>
2018-10-07 11:08:22 +0900

    Check rest parameters types

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,3 +1,11 @@
 "use strict";
-var f1 = function () { };
-f1('foo', 3);
+var func = function (foo) {
+    var bar = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        bar[_i - 1] = arguments[_i];
+    }
+    return bar;
+};
+console.log(func('foo'));
+console.log(func('bar', 1, 2, 3));
+console.log(func('baz', 'hey', 2, 3));
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,3 +1,5 @@
-const f1: (foo: string) => void = () => {}
+const func = (foo: string, ...bar: number[]) => bar
 
-f1('foo', 3)
+console.log(func('foo'))
+console.log(func('bar', 1, 2, 3))
+console.log(func('baz', 'hey', 2, 3))
