195b4d7d329657c2264c41b37292cae996073834
yinm <yinm.cminor9@gmail.com>
2018-10-07 11:11:54 +0900

    Check void

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,3 +1,11 @@
 "use strict";
-var a = undefined;
-var b = a;
+function foo() {
+    console.log('hello');
+}
+function bar() {
+    console.log('world');
+}
+function baz() {
+    console.log('typescript');
+    return undefined;
+}
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,2 +1,12 @@
-const a: void = undefined
-const b: undefined = a
+function foo(): void {
+  console.log('hello')
+}
+
+function bar(): undefined {
+  console.log('world')
+}
+
+function baz(): void {
+  console.log('typescript')
+  return undefined
+}
