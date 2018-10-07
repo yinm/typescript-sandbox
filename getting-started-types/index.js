79e8d8fc4db37e23d3d8e3120e334e0aaf15b023
yinm <yinm.cminor9@gmail.com>
2018-10-07 11:57:06 +0900

    Check tuple with generics

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,4 +1,15 @@
 "use strict";
-var func = function (str, num, b) { return args[0] + args[1]; };
-var args = ['foo', 3, false];
-func.apply(void 0, args);
+function bind(func, value) {
+    return function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        return func.apply(void 0, [value].concat(args));
+    };
+}
+var add = function (x, y) { return x + y; };
+var add1 = bind(add, 1);
+console.log(add1(5));
+console.log(add1(5));
+add1('foo');
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,5 +1,13 @@
-const func = (str: string, num: number, b: boolean) => args[0] + args[1]
+function bind<T, U extends any[], R>(
+  func: (arg1: T, ...rest: U) => R,
+  value: T
+): ((...args: U) => R) {
+  return (...args: U) => func(value, ...args)
+}
 
-const args: [string, number, boolean] = ['foo', 3, false]
+const add = (x: number, y: number) => x + y
+const add1 = bind(add, 1)
+console.log(add1(5))
+console.log(add1(5))
 
-func(...args)
+add1('foo')
