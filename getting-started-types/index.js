704c51c9dbb6f3b9507f2871c8b61e515e596137
yinm <yinm.cminor9@gmail.com>
2018-10-07 13:38:03 +0900

    Check never

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,14 +1,2 @@
 "use strict";
-function map(obj, f) {
-    switch (obj.type) {
-        case 'Some':
-            return {
-                type: 'Some',
-                value: f(obj.value)
-            };
-        case 'None':
-            return {
-                type: 'None'
-            };
-    }
-}
+var n = 0;
