ef15479bf71234347cb1c62e7c4bf33e4077d776
yinm <yinm.cminor9@gmail.com>
2018-10-07 11:18:38 +0900

    Check class

--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,7 +1,12 @@
+interface MyFoo {
+  method: () => void
+}
+
 class Foo {
   method(): void {
     console.log('Hello, world!')
   }
 }
 
-const obj: Foo = new Foo()
+const obj: MyFoo = new Foo()
+const obj2: Foo = obj
