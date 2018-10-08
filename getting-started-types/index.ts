edd264f0dd530dd0997543d48ab5ae190725c5a7
yinm <yinm.cminor9@gmail.com>
2018-10-08 14:17:27 +0900

    Check 2nd

--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,12 +1,14 @@
-interface MyFoo {
-  method: () => void
+interface Foo<S, T> {
+  foo: S
+  bar: T
 }
 
-class Foo {
-  method(): void {
-    console.log('Hello world')
-  }
+const obj: Foo<number, string> = {
+  foo: 3,
+  bar: 'hi'
 }
 
-const obj: MyFoo = new Foo()
-const obj2: Foo = obj
+const obj2: Foo<string, number> = {
+  foo: 'aaa',
+  bar: 5
+}
