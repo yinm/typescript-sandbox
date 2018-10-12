6f8ed48775a6565ba9bdfff45352251ec516df30
yinm <yinm.cminor9@gmail.com>
2018-10-12 19:58:18 +0900

    Check generics

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,11 +1,5 @@
 "use strict";
-var Foo = /** @class */ (function () {
-    function Foo() {
-    }
-    Foo.prototype.method = function () {
-        console.log('Hello, world');
-    };
-    return Foo;
-}());
-var obj = new Foo();
-var obj2 = obj;
+var obj = {
+    foo: 3,
+    bar: 'hi'
+};
