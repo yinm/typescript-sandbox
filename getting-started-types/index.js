d3dcdb90687a7fa18b93f9edb21ca584ecca21d6
yinm <yinm.cminor9@gmail.com>
2018-10-12 20:01:45 +0900

    Check generics

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,9 +1,3 @@
 "use strict";
-var Foo = /** @class */ (function () {
-    function Foo(obj) {
-    }
-    return Foo;
-}());
-var obj1 = new Foo('foo');
 function func(obj) { }
-func(3);
+var f = func;
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,7 +1,2 @@
-class Foo<T> {
-  constructor(obj: T) {}
-}
-const obj1 = new Foo<string>('foo')
-
 function func<T>(obj: T): void {}
-func<number>(3)
+const f: <T>(obj: T) => void = func
