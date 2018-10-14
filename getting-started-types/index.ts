23de2f33ee4ea123b91bce115fe45b57e4d69135
yinm <yinm.cminor9@gmail.com>
2018-10-14 10:38:53 +0900

    Check union types

--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,11 +1,4 @@
-function bind<T, U extends any[], R>(
-  func: (arg1: T, ...rest: U) => R,
-  value: T,
-): ((...args: U) => R) {
-  return (...args: U) => func(value, ...args)
-}
-
-const add = (x: number, y: number) => x + y
-const add1 = bind(add, 1)
-console.log(add1(5))
-add1('foo')
+let value: string | number = 'foo'
+value = 100
+value = 'bar'
+value = true
