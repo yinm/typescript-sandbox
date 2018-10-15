aeacc08c31b700668398ca7d462cdc654e2f3d80
yinm <yinm.cminor9@gmail.com>
2018-10-16 07:58:06 +0900

    Check never

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,2 +1,16 @@
 "use strict";
-var foo = n;
+function map(obj, f) {
+    switch (obj.type) {
+        case 'Some':
+            return {
+                type: 'Some',
+                value: f(obj.value)
+            };
+        case 'None':
+            return {
+                type: 'None'
+            };
+        default:
+            return obj;
+    }
+}
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,2 +1,26 @@
-declare const n: never
-const foo: string = n
+interface Some<T> {
+  type: 'Some'
+  value: T
+}
+
+interface None {
+  type: 'None'
+}
+
+type Option<T> = Some<T> | None
+
+function map<T, U>(obj: Option<T>, f: (obj: T) => U): Option<U> {
+  switch (obj.type) {
+    case 'Some':
+      return {
+        type: 'Some',
+        value: f(obj.value)
+      }
+    case 'None':
+      return {
+        type: 'None'
+      }
+    default:
+      return obj
+  }
+}
