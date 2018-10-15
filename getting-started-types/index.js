11a159573fa293db853f5aa87fcd0c0cad2928b5
yinm <yinm.cminor9@gmail.com>
2018-10-16 07:59:27 +0900

    Check never

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,16 +1,5 @@
 "use strict";
-function map(obj, f) {
-    switch (obj.type) {
-        case 'Some':
-            return {
-                type: 'Some',
-                value: f(obj.value)
-            };
-        case 'None':
-            return {
-                type: 'None'
-            };
-        default:
-            return obj;
-    }
+function func() {
+    throw new Error('Hi');
 }
+var result = func();
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,26 +1,5 @@
-interface Some<T> {
-  type: 'Some'
-  value: T
+function func(): never {
+  throw new Error('Hi')
 }
 
-interface None {
-  type: 'None'
-}
-
-type Option<T> = Some<T> | None
-
-function map<T, U>(obj: Option<T>, f: (obj: T) => U): Option<U> {
-  switch (obj.type) {
-    case 'Some':
-      return {
-        type: 'Some',
-        value: f(obj.value)
-      }
-    case 'None':
-      return {
-        type: 'None'
-      }
-    default:
-      return obj
-  }
-}
+const result: never = func()
