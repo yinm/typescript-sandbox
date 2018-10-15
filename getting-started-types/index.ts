efa4f025bf367f60f54ffd720341d8e1465672e4
yinm <yinm.cminor9@gmail.com>
2018-10-16 08:04:16 +0900

    Check intersection with union

--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,15 +1,28 @@
 interface Hoge {
+  type: 'hoge'
   foo: string
-  bar: number
 }
 
 interface Piyo {
-  foo: string
+  type: 'piyo'
+  bar: number
+}
+
+interface Fuga {
   baz: boolean
 }
 
-const obj: Hoge & Piyo = {
-  foo: 'fooooo',
-  bar: 3,
-  baz: true
+type Obj = (Hoge | Piyo) & Fuga
+
+function func(obj: Obj) {
+  console.log(obj.baz)
+
+  if (obj.type === 'hoge') {
+    console.log(obj.foo)
+  } else {
+    console.log(obj.bar)
+  }
 }
+
+func({type: 'hoge', foo: 'foooooo', baz: true})
+func({type: 'piyo', bar: 20, baz: false})
