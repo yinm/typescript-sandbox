be003214445ae15fe53b0fa7260f0a530050115f
yinm <yinm.cminor9@gmail.com>
2018-10-17 22:15:02 +0900

    Check ?

--- getting-started-types/index.js
+++ getting-started-types/index.js
@@ -1,12 +1,8 @@
 "use strict";
-function func(obj) {
-    console.log(obj.baz);
-    if (obj.type === 'hoge') {
-        console.log(obj.foo);
-    }
-    else {
-        console.log(obj.bar);
-    }
-}
-func({ type: 'hoge', foo: 'foooooo', baz: true });
-func({ type: 'piyo', bar: 20, baz: false });
+var obj = {
+    foo: 'string'
+};
+obj = {
+    foo: 'foo',
+    bar: 100
+};
--- getting-started-types/index.ts
+++ getting-started-types/index.ts
@@ -1,28 +1,13 @@
-interface Hoge {
-  type: 'hoge'
+interface MyObj {
   foo: string
+  bar?: number
 }
 
-interface Piyo {
-  type: 'piyo'
-  bar: number
+let obj: MyObj = {
+  foo: 'string'
 }
 
-interface Fuga {
-  baz: boolean
+obj = {
+  foo: 'foo',
+  bar: 100
 }
-
-type Obj = (Hoge | Piyo) & Fuga
-
-function func(obj: Obj) {
-  console.log(obj.baz)
-
-  if (obj.type === 'hoge') {
-    console.log(obj.foo)
-  } else {
-    console.log(obj.bar)
-  }
-}
-
-func({type: 'hoge', foo: 'foooooo', baz: true})
-func({type: 'piyo', bar: 20, baz: false})
