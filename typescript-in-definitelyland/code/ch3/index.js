da9a98f89c6cc6e51439d013e7675e3d6bda8849
yinm <yinm.cminor9@gmail.com>
2018-11-23 12:35:10 +0900

    Check Property Signatures

--- typescript-in-definitelyland/code/ch3/index.js
+++ typescript-in-definitelyland/code/ch3/index.js
@@ -1,13 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function move(value, delta) {
-    if (delta.dx) {
-        value.x += delta.dx;
-    }
-    if (delta.dy) {
-        value.y += delta.dy;
-    }
-    return value;
-}
-let result = move({ x: 1, y: 2 }, { dx: -2 });
-console.log(JSON.stringify(result, null, 2));
+let obj;
+obj = {
+    property: 'Hi!'
+};
+console.log(obj);
--- typescript-in-definitelyland/code/ch3/index.ts
+++ typescript-in-definitelyland/code/ch3/index.ts
@@ -1,18 +1,11 @@
-function move(
-  value: { x: number; y: number; },
-  delta: { dx?: number; dy?: number; }
-): { x: number; y: number } {
-  if (delta.dx) {
-    value.x += delta.dx
-  }
-  if (delta.dy) {
-    value.y += delta.dy
-  }
-
-  return value
+let obj: {
+  property: string;
 }
 
-let result = move({ x: 1, y: 2 }, { dx: -2 })
-console.log(JSON.stringify(result, null, 2))
+obj = {
+  property: 'Hi!'
+}
+
+console.log(obj)
 
 export { }
