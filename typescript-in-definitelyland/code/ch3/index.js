c180a922e30de915d4c709829041cda00fddf4ae
yinm <yinm.cminor9@gmail.com>
2018-11-23 14:28:44 +0900

    Check any

--- typescript-in-definitelyland/code/ch3/index.js
+++ typescript-in-definitelyland/code/ch3/index.js
@@ -1,29 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Base {
-    constructor(str) {
-        this.str = str;
-    }
-}
-class InheritA extends Base {
-    constructor(num) {
-        super('TypeScript');
-        this.num = num;
-    }
-}
-class InheritB extends Base {
-    constructor(bool) {
-        super('TypeScript');
-        this.bool = bool;
-    }
-}
-let obj = new InheritA(1);
-if (obj instanceof InheritA) {
-    obj.num;
-}
-else if (obj instanceof InheritB) {
-    obj.bool;
-}
-else {
-    obj.str;
-}
+let obj = new Base();
+let num = obj.num;
+exports.num = num;
--- typescript-in-definitelyland/code/ch3/index.ts
+++ typescript-in-definitelyland/code/ch3/index.ts
@@ -1,27 +1,8 @@
-class Base {
-  constructor(public str: string) {}
+declare class Base {
+  str: string;
 }
 
-class InheritA extends Base {
-  constructor(public num: number) {
-    super('TypeScript')
-  }
-}
+let obj = new Base()
+let num: number = (obj as any).num
 
-class InheritB extends Base {
-  constructor(public bool: boolean) {
-    super('TypeScript')
-  }
-}
-
-let obj: Base = new InheritA(1)
-
-if (obj instanceof InheritA) {
-  obj.num
-} else if (obj instanceof InheritB) {
-  obj.bool
-} else {
-  obj.str
-}
-
-export { }
+export { num }
