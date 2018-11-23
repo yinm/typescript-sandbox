df795cc1e40e0a699bb06aad21146e76082c701c
yinm <yinm.cminor9@gmail.com>
2018-11-23 10:38:51 +0900

    Check 3rd

--- typescript-in-definitelyland/code/sample.js
+++ typescript-in-definitelyland/code/sample.js
@@ -1,8 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-// 2nd
+// 3rd
 function asyncModoki(callback) {
     callback('TypeScript');
 }
 asyncModoki(value => console.log(`Hello, ${value}`));
 asyncModoki((value) => console.log(`Hello, ${value}`));
+asyncModoki(function (value) { console.log(`Hello, ${value}`); });
--- typescript-in-definitelyland/code/sample.ts
+++ typescript-in-definitelyland/code/sample.ts
@@ -1,9 +1,10 @@
-// 2nd
+// 3rd
 function asyncModoki(callback: (value: string) => void) {
   callback('TypeScript')
 }
 
 asyncModoki(value => console.log(`Hello, ${value}`))
 asyncModoki((value: string): void => console.log(`Hello, ${value}`))
+asyncModoki(function(value: string): void { console.log(`Hello, ${value}`) })
 
 export { }
