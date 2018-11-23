470ff3f95e40eb880d9f38c129323d4791a4c498
yinm <yinm.cminor9@gmail.com>
2018-11-23 14:52:02 +0900

    Check generics

--- typescript-in-definitelyland/code/ch3/index.js
+++ typescript-in-definitelyland/code/ch3/index.js
@@ -1,5 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let obj = new Base();
-let num = obj.num;
-exports.num = num;
+class SampleA {
+    constructor(data) {
+        this.data = data;
+    }
+}
+exports.SampleA = SampleA;
+let objA = new SampleA('str');
+exports.objA = objA;
+let objB = { data: 1 };
+exports.objB = objB;
+let obj;
+exports.obj = obj;
+let func;
+func = (array) => array[0];
+func([1, 2, 3]);
+let ctor;
+ctor = SampleA;
+new ctor('str');
+let objC = { data: 1 };
+exports.objC = objC;
--- typescript-in-definitelyland/code/ch3/index.ts
+++ typescript-in-definitelyland/code/ch3/index.ts
@@ -1,8 +1,29 @@
-declare class Base {
-  str: string;
+class SampleA<T> {
+  constructor(public data: T) {}
+}
+let objA = new SampleA<string>('str')
+
+interface SampleB<T> {
+  data: T;
+}
+let objB: SampleB<number> = { data: 1 }
+
+let obj: {
+  new <T>(value: T): any;
+  <T>(value: T): any;
+  methodA<T>(value: T): any;
+  methodB<T, U>(value: T): U;
 }
 
-let obj = new Base()
-let num: number = (obj as any).num
+let func: <T>(array: T[]) => T
+func = <T>(array: T[]) => array[0]
+func<number>([1, 2, 3])
 
-export { num }
+let ctor: new <T>(value: T) => any
+ctor = SampleA
+new ctor<string>('str')
+
+type SampleC<T> = { data: T }
+let objC: SampleC<number> = { data: 1 }
+
+export { SampleA, objA, SampleB, objB, obj, SampleC, objC }
