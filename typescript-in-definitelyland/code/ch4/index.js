a4a9a667b2a3bae06818f8e1a7ba863d54f4669c
yinm <yinm.cminor9@gmail.com>
2018-11-24 11:58:27 +0900

    Check type guard

--- typescript-in-definitelyland/code/ch4/index.js
+++ typescript-in-definitelyland/code/ch4/index.js
@@ -1,9 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let foo = {
-    value: 'TypeScript'
-};
-// foo.value.toUpperCase()
-if (typeof foo.value === 'string') {
-    foo.value.toUpperCase();
+let v1;
+exports.v1 = v1;
+exports.v1 = v1 = 'str';
+if (typeof v1 === 'string') {
+    let f = () => {
+        if (typeof v1 === 'string') {
+            console.log(v1.toUpperCase());
+        }
+    };
+    f();
+    exports.v1 = v1 = 1;
+    f();
+}
+const v2 = null;
+exports.v2 = v2;
+if (typeof v2 === 'string') {
+    let f = () => {
+        console.log(v2.toUpperCase());
+    };
+    f();
 }
--- typescript-in-definitelyland/code/ch4/index.ts
+++ typescript-in-definitelyland/code/ch4/index.ts
@@ -1,15 +1,23 @@
-interface Foo {
-  value: number | string;
+let v1: string | number
+v1 = 'str'
+if (typeof v1 === 'string') {
+  let f = () => {
+    if (typeof v1 === 'string') {
+      console.log(v1.toUpperCase())
+    }
+  }
+  f()
+
+  v1 = 1
+  f()
 }
 
-let foo: Foo = {
-  value: 'TypeScript'
+const v2: string | number = null as any
+if (typeof v2 === 'string') {
+  let f = () => {
+    console.log(v2.toUpperCase())
+  }
+  f()
 }
 
-// foo.value.toUpperCase()
-
-if (typeof foo.value === 'string') {
-  foo.value.toUpperCase()
-}
-
-export { }
+export { v1, v2 }
