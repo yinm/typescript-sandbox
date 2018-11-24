51bb50816d676077932375ee3587603a9c3c81fa
yinm <yinm.cminor9@gmail.com>
2018-11-24 13:07:16 +0900

    Check primitive literal types

--- typescript-in-definitelyland/code/ch4/index.ts
+++ typescript-in-definitelyland/code/ch4/index.ts
@@ -1,7 +1,18 @@
-type FooReturns = string | number | boolean;
+type Suit = 'Heart' | 'Diamond' | 'Club' | 'Spade'
 
-interface Foo {
-  bar(): FooReturns;
-  buzz(): FooReturns;
-  barbuzz(): FooReturns;
+let suit: Suit = 'Heart'
+suit = 'Joker'
+
+type Digit = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7
+type Bool = true | false
+type Falsy = false | 0 | '' | null | undefined
+
+enum SuitEnum {
+  Heart,
+  Diamond,
+  Club,
+  Spade,
 }
+type RedSuit = SuitEnum.Heart | SuitEnum.Diamond
+
+export { Suit, suit, Digit, Bool, Falsy, SuitEnum, RedSuit }
