89dfde10e477ecb8cb3698e3d89d2c033293741d
yinm <yinm.cminor9@gmail.com>
2018-11-24 13:18:50 +0900

    Check primitive literal types

--- typescript-in-definitelyland/code/ch4/index.js
+++ typescript-in-definitelyland/code/ch4/index.js
@@ -1,1 +1,25 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let node = {
+    type: 'add',
+    left: {
+        type: 'add',
+        left: { type: 'leaf', value: 10 },
+        right: { type: 'leaf', value: 3 }
+    },
+    right: {
+        type: 'leaf',
+        value: 5
+    }
+};
+console.log(calc(node));
+function calc(root) {
+    switch (root.type) {
+        case 'leaf':
+            return root.value;
+        case 'add':
+            return calc(root.left) + calc(root.right);
+        default:
+            throw new Error('unknown node');
+    }
+}
--- typescript-in-definitelyland/code/ch4/index.ts
+++ typescript-in-definitelyland/code/ch4/index.ts
@@ -1,18 +1,40 @@
-interface HTMLBodyElement extends HTMLElement {
-  addEventListener(
-    type: 'change',
-    listener: (this: this, ev: Event) => any,
-    useCapture?: boolean): void;
-  addEventListener(
-    type: 'click',
-    listener: (this: this, ev: MouseEvent) => any,
-    useCapture?: boolean): void;
-  addEventListener(
-    type: 'keypress',
-    listener: (this: this, ev: KeyboardEvent) => any,
-    useCapture?: boolean): void;
-  addEventListener(
-    type: string,
-    listener: EventListenerOrEventListenerObject,
-    useCapture?: boolean): void;
+interface Add {
+  type: 'add';
+  left: Tree;
+  right: Tree;
 }
+
+interface Leaf {
+  type: 'leaf';
+  value: number;
+}
+
+type Tree = Add | Leaf;
+
+let node: Tree = {
+  type: 'add',
+  left: {
+    type: 'add',
+    left: { type: 'leaf', value: 10 },
+    right: { type: 'leaf', value: 3 }
+  },
+  right: {
+    type: 'leaf',
+    value: 5
+  }
+}
+
+console.log(calc(node))
+
+function calc(root: Tree): number {
+  switch (root.type) {
+    case 'leaf':
+      return root.value
+    case 'add':
+      return calc(root.left) + calc(root.right)
+    default:
+      throw new Error('unknown node')
+  }
+}
+
+export {}
