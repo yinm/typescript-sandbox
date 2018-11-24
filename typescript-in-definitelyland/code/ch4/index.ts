834da4b29f5f9d77ed50078383e567264b1a5d94
yinm <yinm.cminor9@gmail.com>
2018-11-24 12:58:52 +0900

    Check type alias

--- typescript-in-definitelyland/code/ch4/index.ts
+++ typescript-in-definitelyland/code/ch4/index.ts
@@ -1,48 +1,7 @@
-declare namespace angular.resource1 {
-  interface ResourceProvider {
-    create<T extends Resource<any>>(): T;
-  }
+type FooReturns = string | number | boolean;
 
-  interface Resource<T> {
-    $insert(): T;
-  }
-  let $resource: ResourceProvider
+interface Foo {
+  bar(): FooReturns;
+  buzz(): FooReturns;
+  barbuzz(): FooReturns;
 }
-
-namespace sample1 {
-  interface Sample {
-    str: string;
-  }
-
-  interface SampleResource extends Sample, angular.resource1.Resource<Sample> { }
-
-  let $obj = angular.resource1.$resource.create<SampleResource>()
-  $obj.str = 'test'
-  let obj = $obj.$insert()
-  console.log(obj.str)
-}
-
-
-declare namespace angular.resource2 {
-  interface ResourcePProvider {
-    create<T>(): T & Resource<T>
-  }
-
-  interface Resource<T> {
-    $insert(): T;
-  }
-  let $resource: ResourcePProvider
-}
-
-namespace sample2 {
-  interface Sample {
-    str: string;
-  }
-
-  let $obj = angular.resource2.$resource.create<Sample>()
-  $obj.str = 'test'
-  let obj = $obj.$insert()
-  console.log(obj.str)
-}
-
-export { sample1, sample2 }
