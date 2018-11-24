5d5ad73fabb356104c0f9eebced2672b94420c30
yinm <yinm.cminor9@gmail.com>
2018-11-24 16:19:59 +0900

    Check builtin

--- typescript-in-definitelyland/code/ch4/index.js
+++ typescript-in-definitelyland/code/ch4/index.js
@@ -1,1 +1,28 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let c1 = {
+    kind: 'neko',
+    name: 'アルファ'
+};
+exports.c1 = c1;
+let c2 = {
+    kind: 'neko'
+};
+exports.c2 = c2;
+let c3 = {
+    kind: 'neko',
+    name: 'アルファ',
+    age: 3
+};
+exports.c3 = c3;
+// c3.kind = 'TS'
+let c4 = {
+    name: 'アルファ'
+};
+exports.c4 = c4;
+let c5 = {
+    kind: true,
+    name: true,
+    age: true
+};
+exports.c5 = c5;
--- typescript-in-definitelyland/code/ch4/index.ts
+++ typescript-in-definitelyland/code/ch4/index.ts
@@ -1,7 +1,33 @@
 interface Cat {
   kind: string;
-  name: string;
-  age: number;
+  name: string
+  age?: number;
 }
 
-type CatPropertyNames = keyof Cat
+let c1: Cat = {
+  kind: 'neko',
+  name: 'アルファ'
+}
+
+let c2: Partial<Cat> = {
+  kind: 'neko'
+}
+
+let c3: Readonly<Cat> = {
+  kind: 'neko',
+  name: 'アルファ',
+  age: 3
+}
+// c3.kind = 'TS'
+
+let c4: Pick<Cat, 'name' | 'age'> = {
+  name: 'アルファ'
+}
+
+let c5: Record<keyof Cat, boolean> = {
+  kind: true,
+  name: true,
+  age: true
+}
+
+export { c1, c2, c3, c4, c5 }
