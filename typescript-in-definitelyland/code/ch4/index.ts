f3f11f0ff6ab1b3f883125ef2424461ea8da67e1
yinm <yinm.cminor9@gmail.com>
2018-11-24 16:08:55 +0900

    Check lookup types

--- typescript-in-definitelyland/code/ch4/index.ts
+++ typescript-in-definitelyland/code/ch4/index.ts
@@ -1,32 +1,8 @@
-type Constructor<T> = new (...args: any[]) => T
-
-function Tagged<T extends Constructor<object>>(Base: T) {
-  return class extends Base {
-    tag = ''
-    constructor(...args: any[]) {
-      super(...args)
-    }
-  }
+interface Cat {
+  kind: string;
+  name: string;
+  age: number;
 }
 
-class Score {
-  constructor(public point: number) {}
-}
-
-const TaggedScore = Tagged(Score)
-
-const ts = new TaggedScore(1)
-ts.tag = 'vv'
-console.log(ts.tag, ts.point)
-
-// new TaggedScore('s')
-
-class RankingScore extends TaggedScore {
-  constructor(public rank: number, tag: string, point: number) {
-    super(point)
-    this.tag = tag
-  }
-}
-
-const rs = new RankingScore(1, 'vv', 100)
-console.log(rs.rank, rs.tag, rs.point)
+type KindType = Cat['kind']
+type NameOrAge = Cat['name'|'age']
