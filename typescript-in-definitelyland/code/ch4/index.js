dd10bf597ef87f914519a871fb77da26eb94418d
yinm <yinm.cminor9@gmail.com>
2018-11-24 15:41:42 +0900

    Check tuple

--- typescript-in-definitelyland/code/ch4/index.js
+++ typescript-in-definitelyland/code/ch4/index.js
@@ -1,16 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-class Sample {
-    constructor(str) {
-        this.str = str;
-    }
-}
-let obj;
-exports.obj = obj;
-let clazz;
-exports.obj = obj = new Sample('TS');
-clazz = Sample;
-exports.obj = obj = new clazz('TS');
-let alterClazz;
-alterClazz = clazz;
-clazz = alterClazz;
+let array = [1, 'str', true];
+exports.array = array;
+// array[1].charAt(0)
+let tuple = [1, 'str', true];
+tuple[1].charAt(0);
+tuple.forEach(v => console.log(v));
--- typescript-in-definitelyland/code/ch4/index.ts
+++ typescript-in-definitelyland/code/ch4/index.ts
@@ -1,9 +1,8 @@
-class Sample {
-  hello = (word = 'TypeScrippt') => `Hello, ${word}`
-  bye: typeof Sample.prototype.hello
-}
+let array = [1, 'str', true]
+// array[1].charAt(0)
 
-let obj = new Sample()
-obj.bye = obj.hello
+let tuple: [number, string, boolean] = [1, 'str', true]
+tuple[1].charAt(0)
+tuple.forEach(v => console.log(v))
 
-export { }
+export { array }
