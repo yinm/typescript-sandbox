7c6841ff66eb0358ededbdc335d30daa060a9d5f
yinm <yinm.cminor9@gmail.com>
2018-11-26 21:20:26 +0900

    Check export

--- typescript-in-definitelyland/code/ch7/index.js
+++ typescript-in-definitelyland/code/ch7/index.js
@@ -1,16 +1,10 @@
 "use strict";
-readFileA('./test.txt', data => {
-    console.log(data.toUpperCase());
-});
-readFileB('./test.txt', data => {
-    if (!data) {
-        data = 'not found';
-    }
-    console.log(data.toUpperCase());
-});
-readFileA('./test.txt', () => {
-    console.log('done');
-});
-readFileB('./test.txt', () => {
-    console.log('done');
-});
+Object.defineProperty(exports, "__esModule", { value: true });
+function hello(word = 'TS') {
+    console.log(`Hello, ${word}`);
+}
+exports.hello = hello;
+function bye(word = 'JS') {
+    console.log(`Bye, ${word}`);
+}
+exports.bye = bye;
--- typescript-in-definitelyland/code/ch7/index.ts
+++ typescript-in-definitelyland/code/ch7/index.ts
@@ -1,27 +1,7 @@
-declare function readFileA(
-  filePath: string,
-  listener: (data: string) => void
-): void
+export function hello(word = 'TS') {
+  console.log(`Hello, ${word}`)
+}
 
-declare function readFileB(
-  filePath: string,
-  listener: (data?: string) => void
-): void
-
-readFileA('./test.txt', data => {
-  console.log(data.toUpperCase())
-})
-
-readFileB('./test.txt', data => {
-  if (!data) {
-    data = 'not found'
-  }
-  console.log(data.toUpperCase())
-})
-
-readFileA('./test.txt', () => {
-  console.log('done')
-})
-readFileB('./test.txt', () => {
-  console.log('done')
-})
+export function bye(word = 'JS') {
+  console.log(`Bye, ${word}`)
+}
