8351e4cfc64e79cd5edca9d1c5e426ddd1692230
yinm <yinm.cminor9@gmail.com>
2018-11-26 09:13:40 +0900

    check jQuery type definition

--- typescript-in-definitelyland/code/ch7/index.ts
+++ typescript-in-definitelyland/code/ch7/index.ts
@@ -1,13 +1,35 @@
-declare namespace ghost {
-  interface Test {
-    str: string;
-  }
+interface JQuery {
+  addClass(className: string): JQuery;
+  html(htmlString: string): JQuery;
+  val(): any;
+  empty(): JQuery;
+  append(content1: JQuery, ...content2: any[]): JQuery;
+  appendTo(target: JQuery): JQuery;
 }
 
-let test: ghost.Test
-test = { str: 'aaa' }
-test.str
+interface JQueryStatic {
+  ajax(settings: JQueryAjaxSettings): any;
+  (selector: string, context?: Element): JQuery;
+  (element: Element): JQuery;
+}
 
-// let notExists = ghost
+interface JQueryAjaxSettings {
+  data?: any;
+  type?: string;
+  url?: string;
+}
 
-export { test }
+interface JQueryPromise<T> {
+  state(): string;
+  then<U>(
+    fullfill: (value: T) => U,
+    reject?: (...reasons: any[]) => U
+  ): JQueryPromise<U>;
+}
+
+interface JQueryDeferred<T> extends JQueryPromise<T> {
+  reject(...args: any[]): JQueryDeferred<T>;
+  resolve(value?: T, ...args: any[]): JQueryDeferred<T>;
+}
+
+declare var $: JQueryStatic
