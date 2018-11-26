1eb0c5fcfc9e3f711e9b82e323fbeefc2d2f8f83
yinm <yinm.cminor9@gmail.com>
2018-11-26 09:22:23 +0900

    Check namespace

--- typescript-in-definitelyland/code/ch7/index.ts
+++ typescript-in-definitelyland/code/ch7/index.ts
@@ -1,37 +1,3 @@
-declare namespace jquery {
-  interface Element {
-    addClass(className: string): Element;
-    html(htmlString: string): Element;
-    val(): any;
-    empty(): Element;
-    append(content1: Element, ...content2: any[]): Element;
-    appendTo(target: Element): Element;
-  }
-
-  interface Static {
-    ajax(settings: AjaxSettings): any;
-    (selector: string, context?: Element): Element;
-    (element: Element): Element;
-  }
-
-  interface AjaxSettings {
-    data?: any;
-    type?: string;
-    url?: string;
-  }
-
-  interface Promise<T> {
-    state(): string;
-    then<U>(
-      fullfill: (value: T) => U,
-      reject?: (...reasons: any[]) => U
-    ): Promise<U>
-  }
-
-  interface Deferred<T> extends Promise<T> {
-    reject(...args: any[]): Deferred<T>;
-    resolve(value?: T, ...args: any[]): Deferred<T>;
-  }
+declare namespace foo.bar.buzz {
+  let str: string;
 }
-
-declare var $: jquery.Static
