0a6b465f81c3fcffd21c78582563ba43db1b7ebe
yinm <yinm.cminor9@gmail.com>
2018-11-26 10:07:55 +0900

    Check class

--- typescript-in-definitelyland/code/ch7/index.ts
+++ typescript-in-definitelyland/code/ch7/index.ts
@@ -1,7 +1,30 @@
-declare class TestA {}
-
-declare let TestB: TestBConstructor
-interface TestBConstructor {
-  new(): TestB
+class Person {
+  constructor(public name: string) {}
 }
-interface TestB {}
+interface Person {
+  age: number;
+}
+let p: Person = new Person('TS')
+console.log(p.name, p.age)
+
+interface AnimalConstructor {
+  new(): Animal;
+}
+interface Animal {
+  speak(): string;
+}
+let Animal: AnimalConstructor = class {
+  speak() {
+    return '???'
+  }
+}
+
+class Cat extends Animal {
+  speak() {
+    return 'meow'
+  }
+}
+let cat: Cat = new cat()
+console.log(cat.speak())
+
+export {}
