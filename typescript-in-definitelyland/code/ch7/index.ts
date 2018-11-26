7fb6c223c591aa51a0c11e77b2e13bc96fe7e421
yinm <yinm.cminor9@gmail.com>
2018-11-26 10:00:33 +0900

    Check class

--- typescript-in-definitelyland/code/ch7/index.ts
+++ typescript-in-definitelyland/code/ch7/index.ts
@@ -1,12 +1,7 @@
-declare function assert(value: any, message?: string): void
-declare namespace assert {
-  export function deepEqual(actual: any, expected: any): void
-  export function notDeepEqual(actual: any, expected: any): void
+declare class TestA {}
 
-  export interface Options {
-    assertion?: any;
-    output?: any;
-  }
-
-  export function customize(options: Options): typeof assert
+declare let TestB: TestBConstructor
+interface TestBConstructor {
+  new(): TestB
 }
+interface TestB {}
