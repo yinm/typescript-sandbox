e8a1b6c701f771ec8e30cbe9adb3ef6c8ad587a3
yinm <yinm.cminor9@gmail.com>
2018-11-26 09:27:49 +0900

    Check type

--- typescript-in-definitelyland/code/ch7/index.ts
+++ typescript-in-definitelyland/code/ch7/index.ts
@@ -1,6 +1,8 @@
-declare var assert: {
-  (value: any): void
-  ok(value: any): void
+declare var assert: Assert
+
+interface Assert {
+  (value: any): void;
+  ok(value: any): void;
 }
 
 assert(foo === 'foo')
