62ec9755c48182929eb60e26d4ed391456508af4
yinm <yinm.cminor9@gmail.com>
2018-11-27 21:01:41 +0900

    Check declare

--- type-definition-files/index.js
+++ type-definition-files/index.js
@@ -1,18 +1,2 @@
 "use strict";
-var outer;
-(function (outer) {
-    var local = 1;
-    outer.a = local;
-    let inner;
-    (function (inner) {
-        inner.x = 10;
-    })(inner = outer.inner || (outer.inner = {}));
-})(outer || (outer = {}));
-(function (outer) {
-    var local = 2;
-    outer.b = local;
-    let inner;
-    (function (inner) {
-        inner.y = 20;
-    })(inner = outer.inner || (outer.inner = {}));
-})(outer || (outer = {}));
+test();
--- type-definition-files/index.ts
+++ type-definition-files/index.ts
@@ -1,15 +1,3 @@
-namespace outer {
-  var local = 1
-  export var a = local
-  export namespace inner {
-    export var x = 10
-  }
-}
+declare function test(): any
 
-namespace outer {
-  var local = 2
-  export var b = local
-  export namespace inner {
-    export var y = 20
-  }
-}
+test()
