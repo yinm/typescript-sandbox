09d53500bcd72594730ab57068a68f15f97b2cdc
yinm <yinm.cminor9@gmail.com>
2018-11-27 20:51:22 +0900

    Check root container

--- type-definition-files/index.ts
+++ type-definition-files/index.ts
@@ -1,2 +1,15 @@
-export {}
-var x = 1
+namespace outer {
+  var local = 1
+  export var a = local
+  export namespace inner {
+    export var x = 10
+  }
+}
+
+namespace outer {
+  var local = 2
+  export var b = local
+  export namespace inner {
+    export var y = 20
+  }
+}
