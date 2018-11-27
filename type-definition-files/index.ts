bf06111d9d565fa055c2532fd7b627e718cbd22a
yinm <yinm.cminor9@gmail.com>
2018-11-27 20:37:04 +0900

    Check open-ended

--- type-definition-files/index.ts
+++ type-definition-files/index.ts
@@ -1,2 +1,13 @@
-type X = number
-type X = number
+interface Document {
+  createElement(tagName: any): Element;
+}
+
+interface Document {
+  createElement(tagName: string): HTMLElement;
+}
+
+interface Document {
+  createElement(tagName: 'div'): HTMLDivElement;
+  createElement(tagName: 'span'): HTMLSpanElement;
+  createElement(tagName: 'canvas'): HTMLCanvasElement;
+}
