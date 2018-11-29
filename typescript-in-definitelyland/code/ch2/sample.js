ac282a3e57912c8493d6752700aa4a98de024964
yinm <yinm.cminor9@gmail.com>
2018-11-29 19:03:45 +0900

    Check async/await

--- typescript-in-definitelyland/code/ch2/sample.js
+++ typescript-in-definitelyland/code/ch2/sample.js
@@ -1,7 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function asyncModoki(callback) {
-    callback('TypeScript');
+function returnByPromise(word) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve(word);
+        }, 100);
+    });
 }
-asyncModoki(value => console.log(`Hello, ${value}`));
-asyncModoki((value) => console.log(`Hello, ${value}`));
+async function helloAsync() {
+    console.log('A');
+    const word = await returnByPromise('TypeScript');
+    console.log(word);
+    console.log('B');
+    return `Hello, ${word}`;
+}
+(async () => {
+    const hello = await helloAsync();
+    console.log(hello);
+})();
+helloAsync().then(hello => console.log(hello));
