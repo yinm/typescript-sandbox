b086dfbed73fff93856adc848d7a9ed7ad43aa14
yinm <yinm.cminor9@gmail.com>
2018-11-29 18:54:00 +0900

    Check rest parameters

--- typescript-in-definitelyland/code/ch2/sample.js
+++ typescript-in-definitelyland/code/ch2/sample.js
@@ -1,18 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-function hello(word) {
-    return `Hello, ${word}`;
+function hello(...args) {
+    return `Hello ${args.join(' & ')}`;
 }
-hello('TypeScript');
-function bye(word) {
-    return `Bye, ${word}`;
-}
-bye('TypeScript');
-function hey(word) {
-    return `Hey, ${word || 'TypeScript'}`;
-}
-hey();
-function ahoy(word = 'TypeScript') {
-    return `Ahoy! ${word}`;
-}
-ahoy();
+console.log(hello('TS', 'JS'));
