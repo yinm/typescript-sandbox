636576f97271a03edb9d585b586610022783d2eb
yinm <yinm.cminor9@gmail.com>
2018-11-30 09:29:37 +0900

    Check namespace with import

--- typescript-in-definitelyland/code/ch2/namespace.js
+++ typescript-in-definitelyland/code/ch2/namespace.js
@@ -2,38 +2,15 @@
 var a;
 (function (a) {
     class Sample {
-        hello(word = 'TypeScript') {
-            return `Hello, ${word}`;
-        }
     }
-    a.obj = new Sample();
-})(a || (a = {}));
-(function (a) {
-    function bye(word = 'JavaScript') {
-        return `Bye, ${word}`;
-    }
-    a.bye = bye;
-    // let tmp = new Sample()
+    a.Sample = Sample;
 })(a || (a = {}));
 var b;
 (function (b) {
-    let c;
-    (function (c) {
-        function hello() {
-            return a.obj.hello();
-        }
-        c.hello = hello;
-    })(c = b.c || (b.c = {}));
+    b.objA = new a.Sample();
+    var Sample = a.Sample;
+    b.objB = new Sample();
+    var Test = a.Sample;
+    b.objC = new Test();
+    b.objA = new Test();
 })(b || (b = {}));
-var d;
-(function (d) {
-    var e;
-    (function (e) {
-        function hello() {
-            return a.obj.hello();
-        }
-        e.hello = hello;
-    })(e = d.e || (d.e = {}));
-})(d || (d = {}));
-console.log(b.c.hello());
-console.log(d.e.hello());
--- typescript-in-definitelyland/code/ch2/namespace.ts
+++ typescript-in-definitelyland/code/ch2/namespace.ts
@@ -1,38 +1,19 @@
 namespace a {
-  class Sample {
-    hello(word = 'TypeScript') {
-      return `Hello, ${word}`
-    }
-  }
-
-  export interface Hello {
-    hello(word?: string): string
-  }
-
-  export let obj: Hello = new Sample()
-}
-
-namespace a {
-  export function bye(word = 'JavaScript') {
-    return `Bye, ${word}`
-  }
-
-  // let tmp = new Sample()
+  export class Sample { }
 }
 
 namespace b {
-  export namespace c {
-    export function hello() {
-      return a.obj.hello()
-    }
-  }
+  export let objA: a.Sample
+  objA = new a.Sample()
+
+  import Sample = a.Sample
+  export let objB: Sample
+  objB = new Sample()
+
+  import Test = a.Sample
+  export let objC: Test
+  objC = new Test()
+
+  objA = new Test()
 }
 
-namespace d.e {
-  export function hello() {
-    return a.obj.hello()
-  }
-}
-
-console.log(b.c.hello())
-console.log(d.e.hello())
