05fb93f6c05a332b4050d7ac0dbde3156f0ac6b4
yinm <yinm.cminor9@gmail.com>
2018-12-01 19:13:40 +0900

    Check weak type detection

--- typescript-in-definitelyland/code/ch3/sample.ts
+++ typescript-in-definitelyland/code/ch3/sample.ts
@@ -1,27 +1,10 @@
-let obj: { name: string } = {
-  name: 'TypeScript'
-}
-
-obj = {
-  name: 'JavaScript',
-  // version: '2016'
-}
-
-let tmp = {
-  name: 'JavaScript',
-  version: '2016'
-}
-obj = tmp
-
 interface FooOptions {
   fileName?: string
   checkBar?: boolean
 }
-declare function foo(opts: FooOptions): void
 
-foo({
-  fileName: 'vvakame.txt',
-  checkBar: true
-})
+let obj = {
+  unknownOption: 1
+}
 
-export { obj }
+let fooOpts: FooOptions = obj
