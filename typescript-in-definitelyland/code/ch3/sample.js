76b853153d8e8d41cfeb103a346495e22989c075
yinm <yinm.cminor9@gmail.com>
2018-12-01 19:38:25 +0900

    Check structural subtyping

--- typescript-in-definitelyland/code/ch3/sample.js
+++ typescript-in-definitelyland/code/ch3/sample.js
@@ -1,25 +1,17 @@
 "use strict";
-class PointImpl1 {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
+function printPoint(p) {
+    let message = `x=${p.x}, y=${p.y}`;
+    if (p.color) {
+        message = `${p.color}(${message})`;
     }
+    console.log(message);
 }
-class PointImpl2 {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-function double(p) {
-    return {
-        x: p.x * 2,
-        y: p.y * 2
-    };
-}
-double(new PointImpl1(1, 2));
-double(new PointImpl2(3, 4));
-double({
-    x: 5,
-    y: 6
+printPoint({
+    x: 1,
+    y: 2
+});
+printPoint({
+    x: 1,
+    y: 2,
+    color: 'red',
 });
