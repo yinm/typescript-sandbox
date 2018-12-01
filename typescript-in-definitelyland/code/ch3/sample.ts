e4dee61ae6d1a612dcf65db2b0943841f1ea0efc
yinm <yinm.cminor9@gmail.com>
2018-12-01 18:47:44 +0900

    Check property signatures

--- typescript-in-definitelyland/code/ch3/sample.ts
+++ typescript-in-definitelyland/code/ch3/sample.ts
@@ -1,17 +1,9 @@
-function move(
-  value: { x: number; y: number; },
-  delta: { dx?: number; dy?: number; }
-): { x: number; y: number } {
-  if (delta.dx) {
-    value.x += delta.dx
-  }
-  if (delta.dy) {
-    value.y += delta.dy
-  }
-  return value
+let obj: {
+  property: string
 }
 
-let result = move({x: 1, y: 2}, {dx: -2})
-console.log(JSON.stringify(result, null, 2))
+obj = {
+  property: 'Hi'
+}
 
-export { }
+export { obj}
