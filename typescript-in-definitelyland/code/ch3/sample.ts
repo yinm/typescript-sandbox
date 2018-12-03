d7b0da7a2d1b0d636da922ded62a00a20e05da25
yinm <yinm.cminor9@gmail.com>
2018-12-03 09:09:57 +0900

    Check typee assertions for type declaration

--- typescript-in-definitelyland/code/ch3/sample.ts
+++ typescript-in-definitelyland/code/ch3/sample.ts
@@ -1,29 +1,8 @@
-class Base {
-  constructor(public str: string) {}
+declare class Base {
+  str: string
 }
 
-class InheritA extends Base {
-  constructor(public num: number) {
-    super('TS')
-  }
-}
+let obj = new Base()
+let num: number = (obj as any).num
 
-class InheritB extends Base {
-  constructor(public bool: boolean) {
-    super('TS')
-  }
-}
-
-let obj: Base = new InheritA(1)
-
-// (obj as InheritA).num
-
-if (obj instanceof InheritA) {
-  obj.num
-} else if (obj instanceof InheritB) {
-  obj.bool
-} else {
-  obj.str
-}
-
-export { }
+export { num }
