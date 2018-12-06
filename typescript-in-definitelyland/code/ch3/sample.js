a559f91dc4730eff904d4635a060847b5bbf75fc
yinm <yinm.cminor9@gmail.com>
2018-12-06 18:20:26 +0900

    Check union types

--- typescript-in-definitelyland/code/ch3/sample.js
+++ typescript-in-definitelyland/code/ch3/sample.js
@@ -1,11 +1,1 @@
 "use strict";
-class DataContainer {
-    print() {
-        console.log(this.data);
-    }
-}
-const obj1 = new DataContainer();
-obj1.data = '';
-// obj1.data = 1
-const obj2 = new DataContainer();
-obj2.data = 1;
---
+++ typescript-in-definitelyland/code/ch4/sample.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let a;
+exports.a = a;
+exports.a = a = 'str';
+// a = 1
+let b1;
+exports.b1 = b1;
+let b2;
+exports.b2 = b2;
+let c;
+exports.c = c;
