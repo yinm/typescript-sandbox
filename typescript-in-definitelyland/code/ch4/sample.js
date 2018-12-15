933272261816235bce8774dc2e7067240e49c248
yinm <yinm.cminor9@gmail.com>
2018-12-15 13:17:51 +0900

    Check func

--- typescript-in-definitelyland/code/ch4/sample.js
+++ typescript-in-definitelyland/code/ch4/sample.js
@@ -1,13 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let a;
-exports.a = a;
-exports.a = a = 'str';
-exports.a = a = true;
-// a = 1
-let b1;
-exports.b1 = b1;
-let b2;
-exports.b2 = b2;
+let func;
+exports.func = func;
+let b;
+exports.b = b;
 let c;
 exports.c = c;
+let d;
+exports.d = d;
--- typescript-in-definitelyland/code/ch4/sample.ts
+++ typescript-in-definitelyland/code/ch4/sample.ts
@@ -1,12 +1,8 @@
-let a: string | boolean | undefined
+let func: () => string
 
-a = 'str'
-a = true
-// a = 1
+let b: (() => string) | (() => boolean)
+let c: { (): string } | { (): boolean }
 
-let b1: string | boolean | undefined
-let b2: boolean | number | undefined
+let d: (() => string)
 
-let c: typeof b1 | typeof b2
-
-export { a, b1, b2, c }
+export { func, b, c, d }
