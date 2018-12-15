9a626b469b126db640a97375a7f349c57002fdcf
yinm <yinm.cminor9@gmail.com>
2018-12-15 20:09:11 +0900

    Check type alias

--- typescript-in-definitelyland/code/ch4/sample.js
+++ typescript-in-definitelyland/code/ch4/sample.js
@@ -1,18 +1,1 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var sample1;
-(function (sample1) {
-    let $obj = angular.resource1.$resource.create();
-    $obj.str = 'test';
-    let obj = $obj.$insert();
-    console.log(obj.str);
-})(sample1 || (sample1 = {}));
-exports.sample1 = sample1;
-var sample2;
-(function (sample2) {
-    let $obj = angular.resource2.$resource.create();
-    $obj.str = 'test';
-    let obj = $obj.$insert();
-    console.log(obj.str);
-})(sample2 || (sample2 = {}));
-exports.sample2 = sample2;
