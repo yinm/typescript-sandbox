8657c3ca56d1e6c8b2b44a3f91c784c3c92a1257
yinm <yinm.cminor9@gmail.com>
2018-12-15 22:48:59 +0900

    Check non-null assertion operator

--- typescript-in-definitelyland/code/ch4/sample.js
+++ typescript-in-definitelyland/code/ch4/sample.js
@@ -1,31 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const fs = require("fs");
-let config = {};
-config.filePath = 'settings.json';
-config.verbose = false;
-processA(config);
-function processA(config = {}) {
-    if (fs.existsSync(config.filePath)) {
-        console.log(fs.readFileSync(config.filePath, 'utf8'));
-    }
-}
-function processB(config = {}) {
-    config.filePath = config.filePath || 'settings.json';
-    config.verbose = config.verbose || false;
-    if (fs.existsSync(config.filePath)) {
-        console.log(fs.readFileSync(config.filePath, 'utf8'));
-    }
-    processA(config);
-}
-exports.processB = processB;
-function processC(config = {}) {
-    const fixed = {
-        filePath: config.filePath || 'settings.json',
-        verbose: config.verbose || false
-    };
-    if (fs.existsSync(fixed.filePath)) {
-        console.log(fs.readFileSync(fixed.filePath, 'utf8'));
-    }
-}
-exports.processC = processC;
+let config = {
+    verbose: true
+};
+let defaultConfig = { filePath: 'settings.json', verbose: false };
+let filled = Object.assign({}, defaultConfig, config);
+let fixed = filled;
+exports.fixed = fixed;
+console.log(fixed);
--- typescript-in-definitelyland/code/ch4/sample.ts
+++ typescript-in-definitelyland/code/ch4/sample.ts
@@ -1,30 +1,6 @@
-import * as fs from 'fs'
-
 interface Config {
   filePath?: string | null
   verbose?: boolean
-}
-
-let config: Config = {}
-config.filePath = 'settings.json'
-config.verbose = false
-processA(config)
-
-function processA(config: Config = {}) {
-  if (fs.existsSync(config.filePath!)) {
-    console.log(fs.readFileSync(config.filePath!, 'utf8'))
-  }
-}
-
-function processB(config: Config = {}) {
-  config.filePath = config.filePath || 'settings.json'
-  config.verbose = config.verbose || false
-
-  if (fs.existsSync(config.filePath)) {
-    console.log(fs.readFileSync(config.filePath, 'utf8'))
-  }
-
-  processA(config)
 }
 
 interface ConfigFixed {
@@ -32,15 +8,14 @@ interface ConfigFixed {
   verbose: boolean
 }
 
-function processC(config: Config = {}) {
-  const fixed: ConfigFixed = {
-    filePath: config.filePath || 'settings.json',
-    verbose: config.verbose || false
-  }
-
-  if (fs.existsSync(fixed.filePath)) {
-    console.log(fs.readFileSync(fixed.filePath, 'utf8'))
-  }
+let config: Config = {
+  verbose: true
 }
 
-export { Config, processB, processC }
+let defaultConfig: ConfigFixed = { filePath: 'settings.json', verbose: false }
+let filled = Object.assign({}, defaultConfig, config)
+
+let fixed: ConfigFixed = filled
+console.log(fixed)
+
+export { ConfigFixed, fixed }
