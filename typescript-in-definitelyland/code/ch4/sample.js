71c676fdf1d4224717d7618fe1a5cd749e800558
yinm <yinm.cminor9@gmail.com>
2018-12-15 21:09:10 +0900

    Check local types

--- typescript-in-definitelyland/code/ch4/sample.js
+++ typescript-in-definitelyland/code/ch4/sample.js
@@ -1,21 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-{
-    let obj = 1;
-    console.log(obj);
-}
-{
-    let obj = 1;
-    console.log(obj);
-}
-// let obj: Data
-{
-    class Foo {
+class Foo {
+    method() {
+        let obj = 1;
+        console.log(obj);
     }
-    let Bar;
-    (function (Bar) {
-        Bar[Bar["a"] = 0] = "a";
-        Bar[Bar["b"] = 1] = "b";
-    })(Bar || (Bar = {}));
-    console.log(Foo, Bar.a, null);
 }
+exports.Foo = Foo;
