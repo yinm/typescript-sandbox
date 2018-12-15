d5af2658cb70b109cd90c8dba24f9f71fd7ed58f
yinm <yinm.cminor9@gmail.com>
2018-12-15 22:45:01 +0900

    Check Non-null Assertion Operator

--- typescript-in-definitelyland/code/ch4/sample.js
+++ typescript-in-definitelyland/code/ch4/sample.js
@@ -1,5 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let tuple = ['str', 1];
-tuple.unshift(1);
-tuple[0].charAt(0);
+const fs = require("fs");
+let config = {};
+config.filePath = 'settings.json';
+config.verbose = false;
+processA(config);
+function processA(config = {}) {
+    if (fs.existsSync(config.filePath)) {
+        console.log(fs.readFileSync(config.filePath, 'utf8'));
+    }
+}
+function processB(config = {}) {
+    config.filePath = config.filePath || 'settings.json';
+    config.verbose = config.verbose || false;
+    if (fs.existsSync(config.filePath)) {
+        console.log(fs.readFileSync(config.filePath, 'utf8'));
+    }
+    processA(config);
+}
+exports.processB = processB;
+function processC(config = {}) {
+    const fixed = {
+        filePath: config.filePath || 'settings.json',
+        verbose: config.verbose || false
+    };
+    if (fs.existsSync(fixed.filePath)) {
+        console.log(fs.readFileSync(fixed.filePath, 'utf8'));
+    }
+}
+exports.processC = processC;
--- typescript-in-definitelyland/code/ch4/sample.ts
+++ typescript-in-definitelyland/code/ch4/sample.ts
@@ -1,7 +1,46 @@
-let tuple: [string, number] = ['str', 1]
+import * as fs from 'fs'
 
-tuple.unshift(1)
+interface Config {
+  filePath?: string | null
+  verbose?: boolean
+}
 
-tuple[0].charAt(0)
+let config: Config = {}
+config.filePath = 'settings.json'
+config.verbose = false
+processA(config)
 
-export { }
+function processA(config: Config = {}) {
+  if (fs.existsSync(config.filePath!)) {
+    console.log(fs.readFileSync(config.filePath!, 'utf8'))
+  }
+}
+
+function processB(config: Config = {}) {
+  config.filePath = config.filePath || 'settings.json'
+  config.verbose = config.verbose || false
+
+  if (fs.existsSync(config.filePath)) {
+    console.log(fs.readFileSync(config.filePath, 'utf8'))
+  }
+
+  processA(config)
+}
+
+interface ConfigFixed {
+  filePath: string
+  verbose: boolean
+}
+
+function processC(config: Config = {}) {
+  const fixed: ConfigFixed = {
+    filePath: config.filePath || 'settings.json',
+    verbose: config.verbose || false
+  }
+
+  if (fs.existsSync(fixed.filePath)) {
+    console.log(fs.readFileSync(fixed.filePath, 'utf8'))
+  }
+}
+
+export { Config, processB, processC }
