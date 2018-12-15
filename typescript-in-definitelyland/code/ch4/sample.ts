d26002a4bcfa3fcfe3cec526e13967399e589436
yinm <yinm.cminor9@gmail.com>
2018-12-15 23:15:52 +0900

    Check mapped types

--- typescript-in-definitelyland/code/ch4/sample.ts
+++ typescript-in-definitelyland/code/ch4/sample.ts
@@ -1,7 +1,33 @@
 interface Cat {
   kind: string
   name: string
-  age: number
+  age?: number
 }
 
-type CatPropertyNmaes = keyof Cat
+let c1: Cat = {
+  kind: 'NorwegianForestCat',
+  name: 'アルファ'
+}
+
+let c2: Partial<Cat> = {
+  kind: 'NorwegianForestCat'
+}
+
+let c3: Readonly<Cat> = {
+  kind: 'NorwegianForestCat',
+  name: 'アルファ',
+  age: 3
+}
+// c3.kind = 'TypeScript'
+
+let c4: Pick<Cat, 'name' | 'age'> = {
+  name: 'アルファ'
+}
+
+let c5: Record<keyof Cat, boolean> = {
+  kind: true,
+  name: true,
+  age: true
+}
+
+export { c1, c2, c3, c4, c5 }
